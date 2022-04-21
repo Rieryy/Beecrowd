24b78fb014a5c8eea33368d453e4b3e70556d888
Riery <Rieryart@gmail.com>
2022-04-21 10:52:11 -0300

    Acrescentado a questão 1001

---
+++ 1000/1001/1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _1001 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine("X = " + sum);
+
+        }
+    }
+}
